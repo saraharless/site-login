bb36f4c10f11f8028d71a0a1ccc4309c382ec2a1
saraharless <sara.e.harless@gmail.com>
2017-08-17 16:15:37 -0500

    username and password redirect

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 console.log('ghost toast');
 
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
 // const session = require('express-session');
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}))
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
 
+
 app.get('/', function(req,res) {
   res.render('login')
+})
+
+app.post('/login', function(req,res){
+  console.log("username is " + req.body.username);
+  console.log("password is " + req.body.password);
+  res.render('home')
 })
 
 app.listen(3000, function () {
