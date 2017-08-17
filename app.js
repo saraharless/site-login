ad12fd21a76bcee5f6f1bf23eaaec7ce381b659f
saraharless <sara.e.harless@gmail.com>
2017-08-17 15:50:45 -0500

    home page, files connected successfully

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log('ghost toast');
+
+const express = require('express');
+// const bodyParser = require('body-parser');
+const mustacheExpress = require('mustache-express');
+// const session = require('express-session');
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.get('/', function(req,res) {
+  res.send('home')
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+});
